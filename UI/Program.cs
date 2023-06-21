1773eebf6b976e3f3752c3f3eb452508c87fe733
lxymahatma <326265648@qq.com>
2023-06-21 22:00:38 +0900

    move diagnostics outside

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,13 +1,13 @@
-#define WINDOWS
+// #define WINDOWS
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using Avalonia;
 using MuseDashModToolsUI.Models;
 using Serilog;
 using Splat;
 #if DEBUG && WINDOWS
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 #endif
 
