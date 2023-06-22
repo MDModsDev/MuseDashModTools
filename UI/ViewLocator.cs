46f9eb71ef48ab4ca0f8ea63b4645b7ab7eb30f5
lxymahatma <326265648@qq.com>
2023-06-22 09:21:40 +0900

    [skip ci] null checking

--- UI/ViewLocator.cs
+++ UI/ViewLocator.cs
@@ -12,7 +12,7 @@ public class ViewLocator : IDataTemplate
         var name = data?.GetType().FullName!.Replace("ViewModel", "View");
         var type = Type.GetType(name!);
 
-        if (type != null)
+        if (type is not null)
             return (Control)Activator.CreateInstance(type)!;
         return new TextBlock { Text = "Not Found: " + name };
     }
