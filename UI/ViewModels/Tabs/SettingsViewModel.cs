8e03e4f56d76d3360a8e4da81bb2955cd4e89bb7
lxymahatma <326265648@qq.com>
2023-06-27 18:24:27 +0900

    remove property autowired for SettingsViewModel to show the open at start up

--- UI/ViewModels/Tabs/SettingsViewModel.cs
+++ UI/ViewModels/Tabs/SettingsViewModel.cs
@@ -15,7 +15,9 @@ namespace MuseDashModToolsUI.ViewModels.Tabs;
 
 public partial class SettingsViewModel : ViewModelBase, ISettingsViewModel
 {
+    private readonly ILocalizationService _localizationService;
     private readonly ILogger _logger;
+    private readonly IModManageViewModel _modManageViewModel;
     private readonly ISettingService _settingService;
     [ObservableProperty] private string[] _askTypes = { XAML_AskType_Always, XAML_AskType_Yes, XAML_AskType_No };
     [ObservableProperty] private int _currentDownloadSource;
@@ -29,14 +31,15 @@ public partial class SettingsViewModel : ViewModelBase, ISettingsViewModel
     [ObservableProperty] private int _enableDependenciesWhenEnabling;
     [ObservableProperty] private int _enableDependenciesWhenInstalling;
     [ObservableProperty] private string? _path;
-    public ILocalizationService LocalizationService { get; init; }
-    public IModManageViewModel ModManageViewModel { get; init; }
-    public List<Language> AvailableLanguages => LocalizationService.AvailableLanguages;
+    public List<Language> AvailableLanguages => _localizationService.AvailableLanguages;
 
-    public SettingsViewModel(ILogger logger, ISettingService settingService)
+    public SettingsViewModel(ILocalizationService localizationService, ILogger logger, IModManageViewModel modManageViewModel,
+        ISettingService settingService)
     {
-        _settingService = settingService;
+        _localizationService = localizationService;
         _logger = logger;
+        _settingService = settingService;
+        _modManageViewModel = modManageViewModel;
         Initialize();
     }
 
@@ -50,14 +53,14 @@ public partial class SettingsViewModel : ViewModelBase, ISettingsViewModel
     }
 
     [RelayCommand]
-    private void SetLanguage() => LocalizationService.SetLanguage(CurrentLanguage!.Name!);
+    private void SetLanguage() => _localizationService.SetLanguage(CurrentLanguage!.Name!);
 
     [RelayCommand]
     private async Task OnChoosePath()
     {
         _logger.Information("Choose path button clicked");
         var changed = await _settingService.OnChoosePath();
-        if (changed) ModManageViewModel.Initialize();
+        if (changed) _modManageViewModel.Initialize();
     }
 
     #region OnPropertyChanged
