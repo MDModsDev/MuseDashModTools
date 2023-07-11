6e2d39536cdaa753b84d9a2ff2868ea2013dae04
lxymahatma <326265648@qq.com>
2023-07-11 20:59:35 +0900

    Unit test for checking updates

---
+++ UI.Test/GithubServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using MuseDashModToolsUI.Contracts;
+using MuseDashModToolsUI.Services;
+using RichardSzalay.MockHttp;
+using Xunit.Abstractions;
+
+namespace MuseDashModToolsUI.Test;
+
+public class GithubServiceTest
+{
+    private const string ReleaseInfoLink = "https://api.github.com/repos/MDModsDev/MuseDashModToolsUI/releases/latest";
+
+    private const string ReleaseInfo = """
+                                       {
+                                        "tag_name": "v1.10.0-rc1",
+                                        "name": "Test",
+                                        "body": "Test"
+                                       }
+                                       """;
+
+    private readonly MockHttpMessageHandler _client;
+
+    private readonly TestLogger _logger;
+
+    public GithubServiceTest(ITestOutputHelper testOutputHelper)
+    {
+        _client = new MockHttpMessageHandler();
+        _logger = new TestLogger(testOutputHelper);
+    }
+
+    [Fact]
+    public async Task VersionTest()
+    {
+        _client.When(ReleaseInfoLink).Respond("application/json", ReleaseInfo);
+        var githubService = new GitHubService
+        {
+            Logger = _logger,
+            Client = _client.ToHttpClient(),
+            MessageBoxService = new Mock<IMessageBoxService>().Object
+        };
+        await githubService.CheckUpdates();
+    }
+}
---
+++ UI.Test/TestLogger.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Serilog;
+using Serilog.Events;
+using Xunit.Abstractions;
+
+namespace MuseDashModToolsUI.Test;
+
+public class TestLogger : ILogger
+{
+    private ITestOutputHelper TestOutputHelper { get; }
+
+    public TestLogger(ITestOutputHelper testOutputHelper)
+    {
+        TestOutputHelper = testOutputHelper;
+    }
+
+    public void Write(LogEvent logEvent)
+    {
+        TestOutputHelper.WriteLine(logEvent.MessageTemplate.Render(logEvent.Properties, CultureInfo.InvariantCulture));
+        if (logEvent.Exception is not null)
+            TestOutputHelper.WriteLine(logEvent.Exception.ToString());
+    }
+}
