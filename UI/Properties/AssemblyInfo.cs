2c276b520dd038d84b80dd01a0ad4a46a7ffc676
lxymahatma <326265648@qq.com>
2023-09-22 15:53:40 +0900

    use AssemblyInfo so only need to change variable name to apply update version changes

---
+++ UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using MuseDashModToolsUI;
+
+[assembly: AssemblyCopyright(BuildInfo.CopyRight)]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyDescription(BuildInfo.Description)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyTitle(BuildInfo.Description)]
