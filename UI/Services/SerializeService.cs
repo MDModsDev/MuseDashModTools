53abdab3c70d30af1b77d5f4e5d82506e1a0135b
lxymahatma <326265648@qq.com>
2023-09-23 23:44:07 +0900

    use casual encoding in setting

--- UI/Services/SerializeService.cs
+++ UI/Services/SerializeService.cs
@@ -1,3 +1,4 @@
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using MuseDashModToolsUI.Converters;
 
@@ -8,10 +9,20 @@ public class SerializeService : ISerializeService
     public T? DeserializeFromJson<T>(string json, JsonSerializerOptions options) => JsonSerializer.Deserialize<T>(json, options);
 
     public Setting? DeserializeSetting(string json) => DeserializeFromJson<Setting>(
-        json, new JsonSerializerOptions { Converters = { new SemanticVersionJsonConverter() } });
+        json, new JsonSerializerOptions
+        {
+            Converters = { new SemanticVersionJsonConverter() },
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
+        });
 
     public string SerializeToJson(object obj, JsonSerializerOptions options) => JsonSerializer.Serialize(obj, options);
 
     public string SerializeSetting(Setting setting) => SerializeToJson(
-        setting, new JsonSerializerOptions { Converters = { new SemanticVersionJsonConverter() }, WriteIndented = true });
+        setting, new JsonSerializerOptions
+        {
+            Converters = { new SemanticVersionJsonConverter() },
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
+        });
 }
