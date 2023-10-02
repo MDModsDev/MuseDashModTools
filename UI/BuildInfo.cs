9ef0a0163e6dfed03e02c94a49ec0193b1574b88
lxymahatma <326265648@qq.com>
2023-10-02 20:43:16 +0900

    bump version

--- UI/BuildInfo.cs
+++ UI/BuildInfo.cs
@@ -6,5 +6,5 @@ public static class BuildInfo
     public const string CopyRight = "Copyright © 2023 MDModsDev";
     public const string Description = "A tool for Muse Dash modding";
     public const string Name = "MuseDashModTools";
-    public const string Version = "1.2.2";
+    public const string Version = "1.2.3";
 }
