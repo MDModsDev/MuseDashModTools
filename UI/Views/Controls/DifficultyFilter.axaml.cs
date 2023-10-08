0dcff9877cbf4f5e687ea309f0a4cc211a9f5733
lxymahatma <326265648@qq.com>
2023-10-09 02:08:36 +0900

    create DifficultyFilter

---
+++ UI/Views/Controls/DifficultyFilter.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Data;
+
+namespace MuseDashModToolsUI.Views.Controls;
+
+public class DifficultyFilter : MenuItem
+{
+    public static readonly StyledProperty<bool?> IsCheckedProperty =
+        AvaloniaProperty.Register<DifficultyFilter, bool?>(nameof(IsChecked), false,
+            defaultBindingMode: BindingMode.TwoWay);
+
+    public bool? IsChecked
+    {
+        get => GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+}
