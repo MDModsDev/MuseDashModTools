e3390741239fb70223fd0c67a9373e584b80a7ad
lxymahatma <326265648@qq.com>
2023-10-14 02:17:43 +0900

    more styles

---
+++ UI/Views/Controls/Developer.axaml.cs
@@ -0,0 +1,36 @@
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Metadata;
+
+namespace MuseDashModToolsUI.Views.Controls;
+
+public class Developer : TemplatedControl
+{
+    public static readonly StyledProperty<IImage?> SourceProperty =
+        AvaloniaProperty.Register<Image, IImage?>(nameof(Source));
+
+    public static readonly StyledProperty<string?> DeveloperNameProperty =
+        AvaloniaProperty.Register<TextBlock, string?>(nameof(DeveloperName));
+
+    public static readonly StyledProperty<string?> ContributionTextProperty =
+        AvaloniaProperty.Register<TextBlock, string?>(nameof(ContributionText));
+
+    [Content]
+    public IImage? Source
+    {
+        get => GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public string? DeveloperName
+    {
+        get => GetValue(DeveloperNameProperty);
+        set => SetValue(DeveloperNameProperty, value);
+    }
+
+    public string? ContributionText
+    {
+        get => GetValue(ContributionTextProperty);
+        set => SetValue(ContributionTextProperty, value);
+    }
+}
