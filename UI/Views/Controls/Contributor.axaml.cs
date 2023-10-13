5e7fe5b1be6aa1ff4e751603c020581a1f57f6f1
lxymahatma <326265648@qq.com>
2023-10-14 01:42:38 +0900

    add Contributor style

---
+++ UI/Views/Controls/Contributor.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Metadata;
+
+namespace MuseDashModToolsUI.Views.Controls;
+
+public class Contributor : TemplatedControl
+{
+    public static readonly StyledProperty<IImage?> SourceProperty =
+        AvaloniaProperty.Register<Image, IImage?>(nameof(Source));
+
+    public static readonly StyledProperty<string?> TextProperty =
+        AvaloniaProperty.Register<TextBlock, string?>(nameof(Text));
+
+    [Content]
+    public IImage? Source
+    {
+        get => GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
