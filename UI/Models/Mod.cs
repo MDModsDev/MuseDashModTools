80c2c8e7713d8687df1b9836965a6d4535c382ab
lxymahatma <326265648@qq.com>
2023-10-14 19:16:45 +0900

    a bit refactor

--- UI/Models/Mod.cs
+++ UI/Models/Mod.cs
@@ -7,11 +7,19 @@ public partial class Mod : ObservableObject
     [ObservableProperty] private bool _isDisabled;
     public string Name { get; set; } = string.Empty;
     public string Version { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string? DownloadLink { get; set; }
+    public string HomePage { get; set; } = string.Empty;
+    public string[]? GameVersion { get; set; }
+    public string? Description { get; set; }
+    public List<string> DependentMods { get; set; } = new();
+    public List<string> DependentLibs { get; set; } = new();
+    public List<string> IncompatibleMods { get; set; } = new();
+    public string? SHA256 { get; set; }
     [JsonIgnore] public string? LocalVersion { get; set; }
     [JsonIgnore] public UpdateState State { get; set; }
     [JsonIgnore] public bool IsIncompatible { get; set; }
     [JsonIgnore] public bool IsUpdatable => IsLocal && State != UpdateState.Normal;
-    public string Author { get; set; } = string.Empty;
     [JsonIgnore] public string? FileName { get; set; }
     [JsonIgnore] public bool IsLocal => FileName is not null;
     [JsonIgnore] public bool IsInstallable => !IsLocal && !IsIncompatible;
@@ -21,16 +29,12 @@ public partial class Mod : ObservableObject
     [JsonIgnore] public string? DuplicatedModNames { get; set; }
 
     [JsonIgnore]
-    public string XamlDescription => string.Format(XAML_Mod_Description.NormalizeNewline(), Description, Author, Version, CompatibleGameVersion);
-
-    public string? DownloadLink { get; set; }
-    public string? HomePage { get; set; }
+    public string XamlDescription => string.Format(XAML_Mod_Description.NormalizeNewline(),
+        Description, Author, Version, CompatibleGameVersion);
 
     [JsonIgnore]
-    public bool IsValidHomePage => HomePage is not null && Uri.TryCreate(HomePage, UriKind.Absolute, out var uriResult) &&
+    public bool IsValidHomePage => !string.IsNullOrEmpty(HomePage) && Uri.TryCreate(HomePage, UriKind.Absolute, out var uriResult) &&
                                    (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-    public string[]? GameVersion { get; set; }
 
     [JsonIgnore]
     public string CompatibleGameVersion
@@ -42,16 +46,11 @@ public partial class Mod : ObservableObject
         }
     }
 
-    public string? Description { get; set; }
-    public List<string> DependentMods { get; set; } = new();
-    public List<string> DependentLibs { get; set; } = new();
     [JsonIgnore] public int DependencyCount => DependentMods.Count + DependentLibs.Count;
 
     [JsonIgnore]
     public string DependencyNames => DependencyCount == 0 ? string.Empty : string.Join("\r\n", DependentMods.Concat(DependentLibs));
 
-    public List<string> IncompatibleMods { get; set; } = new();
-    public string? SHA256 { get; set; }
     public string FileNameExtended(bool reverse = false) => FileName + ((reverse ? !IsDisabled : IsDisabled) ? ".disabled" : string.Empty);
 
     public void CloneOnlineInfo(Mod webMod)
