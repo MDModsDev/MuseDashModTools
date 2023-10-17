7b98f90a01ede7b646e7c3155ea967f1176acda7
lxymahatma <326265648@qq.com>
2023-10-17 16:20:15 +0900

    implement getmapinfo

--- UI/Services/Partial/ChartService.Partial.cs
+++ UI/Services/Partial/ChartService.Partial.cs
@@ -1,5 +1,24 @@
+using System.IO;
+
 namespace MuseDashModToolsUI.Services;
 
 public partial class ChartService
 {
+    private async Task<MapInfo> GetMapInfo(string filePath)
+    {
+        var mapInfo = new MapInfo();
+        using var sr = new StreamReader(filePath);
+        while (await sr.ReadLineAsync() is { } line)
+        {
+            if (line.Contains("#LEVELDESIGN"))
+                mapInfo.LevelDesigner = line[12..];
+            else if (line.Contains("#PLAYLEVEL"))
+                mapInfo.Difficulty = line[10..];
+
+            if (!string.IsNullOrEmpty(mapInfo.LevelDesigner) && !string.IsNullOrEmpty(mapInfo.Difficulty))
+                return mapInfo;
+        }
+
+        return mapInfo;
+    }
 }
