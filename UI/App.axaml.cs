38bb9c5231bd8a9f267c5dceacbd9e7a7b75be7c
lxymahatma <326265648@qq.com>
2023-10-26 17:30:43 +0900

    add xaml hot reload library

--- UI/App.axaml.cs
+++ UI/App.axaml.cs
@@ -1,10 +1,18 @@
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using HotAvalonia;
 using MuseDashModToolsUI.Views;
 
 namespace MuseDashModToolsUI;
 
 public class App : Application
 {
+    public override void Initialize()
+    {
+        this.EnableHotReload();
+        AvaloniaXamlLoader.Load(this);
+    }
+
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) desktop.MainWindow = new MainWindow();
