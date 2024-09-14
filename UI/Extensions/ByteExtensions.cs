782b2ccd38f4ca3977d40eacb12d3a67f95b0e78
lxymahatma <lxymahatma@gmail.com>
2024-09-14 20:13:48 +0800

    improve ByteExtensions

--- UI/Extensions/ByteExtensions.cs
+++ UI/Extensions/ByteExtensions.cs
@@ -1,28 +1,6 @@
-using System.Text;
-
 namespace MuseDashModToolsUI.Extensions;
 
 public static class ByteExtensions
 {
-    public static string ToHexString(this byte[] bytes)
-    {
-        var sb = new StringBuilder();
-        foreach (var b in bytes)
-        {
-            sb.Append(b.ToString("x2"));
-        }
-
-        return sb.ToString();
-    }
-
-    public static string ToString(this byte[] bytes, string format)
-    {
-        var sb = new StringBuilder();
-        foreach (var b in bytes)
-        {
-            sb.Append(b.ToString(format));
-        }
-
-        return sb.ToString();
-    }
+    public static string ToHexString(this byte[] bytes) => Convert.ToHexString(bytes).ToLowerInvariant();
 }
