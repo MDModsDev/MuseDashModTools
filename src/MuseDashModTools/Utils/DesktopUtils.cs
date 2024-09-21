eeeb07d6d45bbc51d50edae501913c80d152d867
lxymahatma <lxymahatma@gmail.com>
2024-09-21 12:38:56 +0800

    add methods in DesktopUtils

--- src/MuseDashModTools/Utils/DesktopUtils.cs
+++ src/MuseDashModTools/Utils/DesktopUtils.cs
@@ -5,23 +5,47 @@ namespace MuseDashModTools.Utils;
 
 public static class DesktopUtils
 {
-    public static IClassicDesktopStyleApplicationLifetime? GetCurrentDesktop() =>
-        Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
-
-    public static Window GetCurrentMainWindow()
+    public static Application GetCurrentApplication()
     {
-        var desktop = GetCurrentDesktop();
-        if (desktop is null)
+        var app = Application.Current;
+        if (app is null)
+        {
+            throw new InvalidOperationException("Application is null.");
+        }
+
+        return app;
+    }
+
+    public static IClassicDesktopStyleApplicationLifetime GetCurrentDesktop()
+    {
+        if (GetCurrentApplication().ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop)
         {
             throw new InvalidOperationException("Desktop is null.");
         }
 
+        return desktop;
+    }
+
+    public static Window GetCurrentMainWindow()
+    {
+        var desktop = GetCurrentDesktop();
         if (desktop.MainWindow is null)
         {
             throw new InvalidOperationException("MainWindow is null.");
         }
 
         return desktop.MainWindow;
+    }
+
+    public static TopLevel GetCurrentTopLevel()
+    {
+        var mainWindow = GetCurrentMainWindow();
+        if (TopLevel.GetTopLevel(mainWindow) is not { } topLevel)
+        {
+            throw new InvalidOperationException("TopLevel is null.");
+        }
+
+        return topLevel;
     }
 
     public static ILauncher GetLauncher() => GetCurrentMainWindow().Launcher;
