a37071ede85f6ae59dd604f02fba35c08c150573
lxymahatma <lxymahatma@gmail.com>
2024-09-22 21:24:55 +0800

    use tunit in MuseDashModTools.Tests

--- tests/MuseDashModTools.Tests/GlobalUsings.cs
+++ tests/MuseDashModTools.Tests/GlobalUsings.cs
@@ -1,7 +1,5 @@
 global using System.Globalization;
 global using JetBrains.Annotations;
-global using Xunit;
-global using Xunit.Abstractions;
 global using Moq;
 global using MuseDashModTools.Contracts;
 global using MuseDashModTools.Services;
