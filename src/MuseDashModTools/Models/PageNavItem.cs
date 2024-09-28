7669c7fbd38aa127ea921ef0f04253743c50a585
lxymahatma <lxymahatma@gmail.com>
2024-09-29 00:23:18 +0900

    fix navigation command

--- src/MuseDashModTools/Models/PageNavItem.cs
+++ src/MuseDashModTools/Models/PageNavItem.cs
@@ -9,9 +9,18 @@ public sealed class PageNavItem(string name)
     public PageNavItem[] Children { get; set; } = [];
     public string Name { get; set; } = name;
     public bool IsSeparator { get; set; }
-    public bool IsNavigable { get; set; } = true;
+    public bool IsNavigable { get; init; } = true;
     public StreamGeometry? Icon { get; set; }
-    public ICommand NavigationCommand => new RelayCommand(() => WeakReferenceMessenger.Default.Send(Name));
+
+    public ICommand NavigationCommand => new RelayCommand(() =>
+    {
+        if (!IsNavigable)
+        {
+            return;
+        }
+
+        WeakReferenceMessenger.Default.Send(Name);
+    });
 
     public static implicit operator PageNavItem(string name) => new(name);
 }
