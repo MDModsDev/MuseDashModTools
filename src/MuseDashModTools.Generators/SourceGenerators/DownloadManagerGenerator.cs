e21375dd069c051c78a6f01b6b7dec1f64aac4f2
lxymahatma <lxymahatma@gmail.com>
2024-09-29 16:37:19 +0900

    use source generator to generate methods in DownloadManager

---
+++ src/MuseDashModTools.Generators/SourceGenerators/DownloadManagerGenerator.cs
@@ -0,0 +1,95 @@
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace MuseDashModTools.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public sealed class DownloadManagerGenerator : IIncrementalGenerator
+{
+    private static readonly ImmutableArray<string> IgnoredMethodNames = ["CheckForUpdatesAsync"];
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(
+            context.SyntaxProvider.CreateSyntaxProvider(
+                FilterNode, ExtractDataFromContext),
+            GenerateFromData);
+    }
+
+    private static bool FilterNode(SyntaxNode node, CancellationToken _) =>
+        node is InterfaceDeclarationSyntax { Identifier.Text: "IDownloadService" };
+
+    private static IDownloadServiceData? ExtractDataFromContext(GeneratorSyntaxContext context, CancellationToken _)
+    {
+        if (context is not
+            {
+                Node: InterfaceDeclarationSyntax interfaceDeclaration,
+                SemanticModel: var semanticModel
+            })
+        {
+            return null;
+        }
+
+        var symbol = semanticModel.GetDeclaredSymbol(interfaceDeclaration)!;
+        var methods = symbol.GetMembers().OfType<IMethodSymbol>();
+
+        var methodDeclarationDict = new Dictionary<string, MethodData>();
+
+        foreach (var method in methods)
+        {
+            var methodName = method.Name;
+            var parameters = string.Join(", ", method.Parameters.Select(x => x.ToString()));
+            var parameterNames = string.Join(", ", method.Parameters.Select(x => x.Name));
+            var returnType = method.ReturnType.ToDisplayString();
+            methodDeclarationDict.Add(methodName, new MethodData(methodName, parameters, parameterNames, returnType));
+        }
+
+        return new IDownloadServiceData(methodDeclarationDict);
+    }
+
+    private static void GenerateFromData(SourceProductionContext spc, IDownloadServiceData? data)
+    {
+        if (data is not var (methodDeclarationDict))
+        {
+            return;
+        }
+
+        foreach (var ignoredMethodName in IgnoredMethodNames)
+        {
+            methodDeclarationDict.Remove(ignoredMethodName);
+        }
+
+        var sb = new StringBuilder();
+        foreach (var value in methodDeclarationDict.Values)
+        {
+            sb.AppendLine($"{GetGeneratedCodeAttribute(nameof(DownloadManagerGenerator))}");
+            sb.AppendLine($"public {value.ReturnType} {value.MethodName}({value.MethodParameters})");
+            sb.AppendLine($$"""
+                            {
+                              return Setting.DownloadSource switch
+                                   {
+                                       DownloadSource.GitHub => GitHubDownloadService.{{value.MethodName}}({{value.MethodParameterNames}}),
+                                       DownloadSource.GitHubMirror => GitHubMirrorDownloadService.{{value.MethodName}}({{value.MethodParameterNames}}),
+                                       DownloadSource.Custom => CustomDownloadService.{{value.MethodName}}({{value.MethodParameterNames}}),
+                                       _ => throw new UnreachableException()
+                                   };
+                            }
+                            """);
+        }
+
+        spc.AddSource("DownloadManager.g.cs",
+            Header +
+            $$"""
+              namespace MuseDashModTools.Services;
+
+              partial class DownloadManager
+              {
+                  {{sb.ToString().TrimEnd()}}
+              }
+              """);
+    }
+
+    private record IDownloadServiceData(Dictionary<string, MethodData> Methods);
+
+    private record MethodData(string MethodName, string MethodParameters, string MethodParameterNames, string ReturnType);
+}
--- src/MuseDashModTools/Services/DownloadServices/DownloadManager.cs
+++ src/MuseDashModTools/Services/DownloadServices/DownloadManager.cs
@@ -1,6 +1,6 @@
 namespace MuseDashModTools.Services;
 
-public sealed class DownloadManager : IDownloadManager
+public sealed partial class DownloadManager : IDownloadManager
 {
     public Task CheckForUpdatesAsync(CancellationToken cancellationToken = default)
     {
@@ -10,53 +10,6 @@ public sealed class DownloadManager : IDownloadManager
             DownloadSource.GitHubMirror => GitHubMirrorDownloadService.CheckForUpdatesAsync(cancellationToken),
             // For Custom Download Source, because they don't choose GitHub for other downloads, so we will check updates from GitHubMirror
             DownloadSource.Custom => GitHubMirrorDownloadService.CheckForUpdatesAsync(cancellationToken),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public Task<bool> DownloadMelonLoaderAsync(
-        EventHandler<DownloadStartedEventArgs> onDownloadStarted,
-        IProgress<double> downloadProgress,
-        CancellationToken cancellationToken = default)
-    {
-        return Setting.DownloadSource switch
-        {
-            DownloadSource.GitHub => GitHubDownloadService.DownloadMelonLoaderAsync(onDownloadStarted, downloadProgress, cancellationToken),
-            DownloadSource.GitHubMirror => GitHubMirrorDownloadService.DownloadMelonLoaderAsync(onDownloadStarted, downloadProgress, cancellationToken),
-            DownloadSource.Custom => CustomDownloadService.DownloadMelonLoaderAsync(onDownloadStarted, downloadProgress, cancellationToken),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public Task<bool> DownloadModAsync(ModDto mod, CancellationToken cancellationToken = default)
-    {
-        return Setting.DownloadSource switch
-        {
-            DownloadSource.GitHub => GitHubDownloadService.DownloadModAsync(mod, cancellationToken),
-            DownloadSource.GitHubMirror => GitHubMirrorDownloadService.DownloadModAsync(mod, cancellationToken),
-            DownloadSource.Custom => CustomDownloadService.DownloadModAsync(mod, cancellationToken),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public Task<bool> DownloadLibAsync(string libName, CancellationToken cancellationToken = default)
-    {
-        return Setting.DownloadSource switch
-        {
-            DownloadSource.GitHub => GitHubDownloadService.DownloadLibAsync(libName, cancellationToken),
-            DownloadSource.GitHubMirror => GitHubMirrorDownloadService.DownloadLibAsync(libName, cancellationToken),
-            DownloadSource.Custom => CustomDownloadService.DownloadLibAsync(libName, cancellationToken),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public IAsyncEnumerable<Mod?> GetModListAsync(CancellationToken cancellationToken = default)
-    {
-        return Setting.DownloadSource switch
-        {
-            DownloadSource.GitHub => GitHubDownloadService.GetModListAsync(cancellationToken),
-            DownloadSource.GitHubMirror => GitHubMirrorDownloadService.GetModListAsync(cancellationToken),
-            DownloadSource.Custom => CustomDownloadService.GetModListAsync(cancellationToken),
             _ => throw new UnreachableException()
         };
     }
