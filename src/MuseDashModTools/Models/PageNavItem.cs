388c057efcf37e69b436959f010469181a57f6b0
lxymahatma <lxymahatma@gmail.com>
2024-10-01 15:03:58 +0900

    add mod develop page icon

--- src/MuseDashModTools/Models/PageNavItem.cs
+++ src/MuseDashModTools/Models/PageNavItem.cs
@@ -4,11 +4,12 @@ namespace MuseDashModTools.Models;
 
 public sealed partial class PageNavItem(string displayName, string key) : ObservableObject
 {
-    public PageNavItem[] Children { get; set; } = [];
+    public PageNavItem[] Children { get; init; } = [];
     public string DisplayName { get; set; } = displayName;
     public string Key { get; set; } = key;
+    public string? Status { get; init; }
     public bool IsNavigable { get; init; } = true;
-    public bool IsSeparator { get; set; }
+    public bool IsSeparator { get; init; }
 
     [RelayCommand]
     private void Navigation()
