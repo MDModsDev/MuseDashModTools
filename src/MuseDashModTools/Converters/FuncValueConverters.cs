c0d218c9bd5eab89e772ae4204f949050fcf6b5d
lxymahatma <lxymahatma@gmail.com>
2024-10-02 22:23:09 +0900

    update all icons

--- src/MuseDashModTools/Converters/FuncValueConverters.cs
+++ src/MuseDashModTools/Converters/FuncValueConverters.cs
@@ -4,7 +4,7 @@ namespace MuseDashModTools.Converters;
 
 public static class FuncValueConverters
 {
-    public static FuncValueConverter<bool, int> IconSizeConverter { get; } = new(b => b ? 20 : 16);
+    public static FuncValueConverter<bool, int> IconSizeConverter { get; } = new(b => b ? 24 : 16);
 
     public static FuncValueConverter<string, StreamGeometry?> PageIconConverter { get; } = new(str =>
     {
