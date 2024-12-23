8878b58ec2ea54fdb22fe8678561867a86bcc019
lxymahatma <lxymahatma@gmail.com>
2024-12-24 01:21:29 +0900

    fix typo

--- src/MuseDashModTools.Models/GitHubResources.cs
+++ src/MuseDashModTools.Models/GitHubResources.cs
@@ -6,7 +6,7 @@ public static class GitHubResources
 {
     public static readonly Dictionary<string, string> ReadmeCache = [];
 
-    public static ImmutableArray<string> CommonReadmeNames { get; } = ["README.md", "readme.md", "Readme.md", "ReadMe.md", "READMe.MD"];
+    public static ImmutableArray<string> CommonReadmeNames { get; } = ["README.md", "readme.md", "Readme.md", "ReadMe.md", "README.MD"];
 
     public static ImmutableArray<string> Branches { get; } = ["main", "master"];
 }
