f6699a5cf6c6d1266717f9947a36bd459e806a57
lxymahatma <lxymahatma@gmail.com>
2025-02-15 23:50:00 +0800

    rename config file

--- src/MuseDashModTools.Core/Services/SavingService/SavingService.cs
+++ src/MuseDashModTools.Core/Services/SavingService/SavingService.cs
@@ -2,27 +2,27 @@ namespace MuseDashModTools.Core;
 
 internal sealed partial class SavingService : ISavingService
 {
-    private const string SettingFileName = "Setting.json";
+    private const string ConfigFileName = "Config.json";
     private static readonly string ConfigFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), nameof(MuseDashModTools));
-    private static readonly string SettingPath = Path.Combine(ConfigFolder, SettingFileName);
+    private static readonly string ConfigPath = Path.Combine(ConfigFolder, ConfigFileName);
 
     public async Task LoadSettingAsync()
     {
-        if (File.Exists(SettingPath))
+        if (File.Exists(ConfigPath))
         {
-            await using var stream = new FileStream(SettingPath, FileMode.Open, FileAccess.Read);
-            var savedSetting = await JsonSerializationService.DeserializeIndentedAsync<Config>(stream).ConfigureAwait(true);
-            if (savedSetting is null)
+            await using var stream = new FileStream(ConfigPath, FileMode.Open, FileAccess.Read);
+            var savedConfig = await JsonSerializationService.DeserializeIndentedAsync<Config>(stream).ConfigureAwait(true);
+            if (savedConfig is null)
             {
                 Logger.ZLogError($"Saved setting is null");
                 await MessageBoxService.ErrorMessageBoxAsync("Failed to load setting, please delete the setting file and restart the application")
                     .ConfigureAwait(true);
-                PlatformService.RevealFile(SettingPath);
+                PlatformService.RevealFile(ConfigPath);
                 return;
             }
 
-            Config.CopyFrom(savedSetting);
-            Logger.ZLogInformation($"Setting loaded from {SettingPath} successfully");
+            Config.CopyFrom(savedConfig);
+            Logger.ZLogInformation($"Setting loaded from {ConfigPath} successfully");
             await CheckValidSettingAsync().ConfigureAwait(true);
         }
         else
@@ -34,9 +34,9 @@ internal sealed partial class SavingService : ISavingService
 
     public async Task SaveSettingAsync()
     {
-        await using var stream = new FileStream(SettingPath, FileMode.OpenOrCreate, FileAccess.Write);
+        await using var stream = new FileStream(ConfigPath, FileMode.OpenOrCreate, FileAccess.Write);
         await JsonSerializationService.SerializeIndentedAsync(stream, Config).ConfigureAwait(false);
-        Logger.ZLogInformation($"Setting saved to {SettingPath} successfully");
+        Logger.ZLogInformation($"Setting saved to {ConfigPath} successfully");
     }
 
     #region Injections
