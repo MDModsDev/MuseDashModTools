c33609ea3c105e8b4c23583ecf1655204742ac40
lxymahatma <lxymahatma@gmail.com>
2025-02-21 19:47:42 +0800

    remove unused converters

--- src/MuseDashModTools/Converters/FuncValueConverters.cs
+++ src/MuseDashModTools/Converters/FuncValueConverters.cs
@@ -16,4 +16,18 @@ public static class FuncValueConverters
 
         return result;
     });
+
+    public static FuncValueConverter<string?, string?> ChartSceneConverter { get; } = new(scene => scene switch
+    {
+        "scene_01" => XAML_Scene_SpaceStation,
+        "scene_02" => XAML_Scene_Retrocity,
+        "scene_03" => XAML_Scene_Castle,
+        "scene_04" => XAML_Scene_RainyNight,
+        "scene_05" => XAML_Scene_Candyland,
+        "scene_06" => XAML_Scene_Oriental,
+        "scene_07" => XAML_Scene_LetsGroove,
+        "scene_08" => XAML_Scene_Touhou,
+        "scene_09" => XAML_Scene_DJMAX,
+        _ => scene
+    });
 }
