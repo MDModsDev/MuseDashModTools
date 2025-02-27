91b0c2fc755c76587ced20baa25ac2c0bd10ffa5
lxymahatma <lxymahatma@gmail.com>
2025-02-27 20:47:43 +0800

    add SettingItem.axaml

---
+++ src/MuseDashModTools.Styles/TemplatedControls/SettingItem.axaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MuseDashModTools.Styles.TemplatedControls;
+
+public sealed class SettingItem : ContentControl
+{
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<SettingItem, string>(nameof(Title));
+
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<SettingItem, string>(nameof(Description));
+
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+}
