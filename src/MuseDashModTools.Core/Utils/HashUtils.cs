8e9c74fb8a0fc01cd6eeed7bf8f57ca50c69ef22
lxymahatma <lxymahatma@gmail.com>
2025-03-13 23:54:50 +0800

    run code cleanup

--- src/MuseDashModTools.Core/Utils/HashUtils.cs
+++ src/MuseDashModTools.Core/Utils/HashUtils.cs
@@ -7,6 +7,6 @@ public static class HashUtils
     public static string ComputeSHA256HashFromPath(string filePath) =>
         SHA256.HashData(File.ReadAllBytes(filePath)).ToHexStringLower();
 
-    public async static Task<string> ComputeSHA256HashFromPathAsync(string filePath) =>
+    public static async Task<string> ComputeSHA256HashFromPathAsync(string filePath) =>
         SHA256.HashData(await File.ReadAllBytesAsync(filePath).ConfigureAwait(false)).ToHexStringLower();
 }
