db2521416c7866e3bcad21853ee9ab3889d1d5e6
KARPED1EM <leever.zzz@gmail.com>
2025-03-14 01:11:35 +0800

    mod develop panel done

--- src/MuseDashModTools/ViewModels/Panels/Modding/ModDevelopPanelViewModel.cs
+++ src/MuseDashModTools/ViewModels/Panels/Modding/ModDevelopPanelViewModel.cs
@@ -1,5 +1,44 @@
 ﻿namespace MuseDashModTools.ViewModels.Panels.Modding;
 
-public sealed class ModDevelopPanelViewModel : ViewModelBase
+public sealed partial class ModDevelopPanelViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    public partial bool DotNetSdkInstalled { get; set; }
+
+    [ObservableProperty]
+    public partial bool ModTemplateInstalled { get; set; }
+
+    public override async Task InitializeAsync()
+    {
+        // TODO
+
+        // For test
+        DotNetSdkInstalled = true;
+        ModTemplateInstalled = false;
+
+        Logger.ZLogInformation($"{nameof(ModDevelopPanelViewModel)} Initialized");
+    }
+
+    [RelayCommand]
+    private async Task InstallDotNetSdk()
+    {
+        // TODO
+
+        DotNetSdkInstalled = true;
+    }
+
+    [RelayCommand]
+    private async Task InstallModTemplate()
+    {
+        // TODO
+
+        ModTemplateInstalled = true;
+    }
+
+    #region Injections
+
+    [UsedImplicitly]
+    public required ILogger<ModDevelopPanelViewModel> Logger { get; init; }
+
+    #endregion Injections
 }
