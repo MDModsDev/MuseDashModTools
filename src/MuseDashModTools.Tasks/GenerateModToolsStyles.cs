8b7fd9d60e2bca21c21cf26d034280c3f320caa7
lxymahatma <lxymahatma@gmail.com>
2025-03-24 20:54:18 +0800

    add tasks debug launchSettings.json, use xml linq instead of stringbuilder

--- src/MuseDashModTools.Tasks/GenerateModToolsStyles.cs
+++ src/MuseDashModTools.Tasks/GenerateModToolsStyles.cs
@@ -3,12 +3,22 @@ namespace MuseDashModTools.Tasks;
 [PublicAPI]
 public sealed class GenerateModToolsStyles : Task
 {
-    private readonly Dictionary<string, (string Comment, List<string> List)> _categories = new()
+    private const string AvaloniaNamespaceUrl = "https://github.com/avaloniaui";
+    private const string SemiNamespaceUrl = "https://irihi.tech/semi";
+    private const string XamlNamespaceUrl = "http://schemas.microsoft.com/winfx/2006/xaml";
+
+    private const string OutputFileName = "ModToolsStyles.axaml";
+
+    private readonly Dictionary<string, CategoryInfo> _categories = new()
     {
-        ["ExtendedControls"] = ("Extended Controls", []),
-        ["TemplatedControls"] = ("Templated Controls", []),
-        ["Styles"] = ("Control Styles", [])
+        ["ExtendedControls"] = new CategoryInfo("Extended Controls"),
+        ["TemplatedControls"] = new CategoryInfo("Templated Controls"),
+        ["Styles"] = new CategoryInfo("Control Styles")
     };
+
+    private readonly XNamespace AvaloniaNamespace = AvaloniaNamespaceUrl;
+    private readonly XNamespace SemiNamespace = SemiNamespaceUrl;
+    private readonly XNamespace XamlNamespace = XamlNamespaceUrl;
 
     [Required]
     public ITaskItem[] AvaloniaResourceFiles { get; set; } = null!;
@@ -17,55 +27,11 @@ public sealed class GenerateModToolsStyles : Task
     {
         try
         {
-            var sb = new IndentedStringBuilder();
-            sb.AppendLine("""
-                          <Styles xmlns="https://github.com/avaloniaui"
-                                  xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-                                  xmlns:semi="https://irihi.tech/semi">
-
-                              <semi:SemiTheme />
-
-                              <Styles.Resources>
-                                  <ResourceDictionary>
-                                      <ResourceDictionary.ThemeDictionaries>
-                                          <ResourceInclude x:Key="Light" Source="/Resources/LightResource.axaml" />
-                                          <ResourceInclude x:Key="Dark" Source="/Resources/DarkResource.axaml" />
-                                      </ResourceDictionary.ThemeDictionaries>
-                                      <ResourceDictionary.MergedDictionaries>
-                                          <ResourceInclude Source="/Resources/SharedResource.axaml" />
-                                      </ResourceDictionary.MergedDictionaries>
-                                  </ResourceDictionary>
-                              </Styles.Resources>
-                          """);
-
-            foreach (var file in AvaloniaResourceFiles)
-            {
-                var directory = Path.GetDirectoryName(file.ItemSpec);
-                var fileName = file.GetMetadata("Filename");
-
-                if (_categories.TryGetValue(directory, out var category))
-                {
-                    category.List.Add($"""<StyleInclude Source="/{directory}/{fileName}.axaml" />""");
-                }
-            }
-
-            sb.IncreaseIndent();
-            foreach (var (_, (comment, lines)) in _categories)
-            {
-                if (lines is [])
-                {
-                    continue;
-                }
-
-                sb.AppendLine();
-                sb.AppendLine($"<!--  {comment}  -->");
-                lines.ForEach(x => sb.AppendLine(x));
-            }
-
-            sb.ResetIndent();
-            sb.Append("</Styles>");
-
-            File.WriteAllText("ModToolsStyles.axaml", sb.ToString());
+            var styles = BuildBaseStyles();
+            ProcessResourceFiles();
+            AppendCategoryStyles(styles);
+            SaveStylesToFile(styles);
+            Log.LogMessage(MessageImportance.High, $"{OutputFileName} generated successfully.");
             return true;
         }
         catch (Exception ex)
@@ -73,5 +39,92 @@ public sealed class GenerateModToolsStyles : Task
             Log.LogErrorFromException(ex);
             return false;
         }
+    }
+
+    private XElement BuildBaseStyles()
+    {
+        var styles = new XElement(AvaloniaNamespace + "Styles",
+            new XAttribute("xmlns", AvaloniaNamespaceUrl),
+            new XAttribute(XNamespace.Xmlns + "x", XamlNamespaceUrl),
+            new XAttribute(XNamespace.Xmlns + "semi", SemiNamespaceUrl));
+
+        styles.Add(new XElement(SemiNamespace + "SemiTheme"));
+        styles.Add(BuildResourcesSection());
+
+        return styles;
+    }
+
+    private XElement BuildResourcesSection() =>
+        new(AvaloniaNamespace + "Styles.Resources",
+            new XElement(AvaloniaNamespace + "ResourceDictionary",
+                new XElement(AvaloniaNamespace + "ResourceDictionary.ThemeDictionaries",
+                    CreateThemeInclude("Light"),
+                    CreateThemeInclude("Dark")),
+                new XElement(AvaloniaNamespace + "ResourceDictionary.MergedDictionaries",
+                    CreateResourceInclude("Shared"))));
+
+    private XElement CreateThemeInclude(string themeName) =>
+        new(AvaloniaNamespace + "ResourceInclude",
+            new XAttribute(XamlNamespace + "Key", themeName),
+            new XAttribute("Source", $"/Resources/{themeName}Resource.axaml"));
+
+    private XElement CreateResourceInclude(string resourceName) =>
+        new(AvaloniaNamespace + "ResourceInclude",
+            new XAttribute("Source", $"/Resources/{resourceName}Resource.axaml"));
+
+    private void ProcessResourceFiles()
+    {
+        foreach (var file in AvaloniaResourceFiles)
+        {
+            var directory = Path.GetDirectoryName(file.ItemSpec);
+            var fileName = file.GetMetadata("Filename");
+            var relativePath = $"/{directory}/{fileName}.axaml";
+
+            if (_categories.TryGetValue(directory, out var category))
+            {
+                category.Sources.Add(relativePath);
+            }
+        }
+    }
+
+    private void AppendCategoryStyles(XElement styles)
+    {
+        foreach (var (_, category) in _categories)
+        {
+            if (category.Sources is [])
+            {
+                continue;
+            }
+
+            styles.Add(new XComment($"  {category.Comment}  "));
+            category.Sources.ForEach(source =>
+                styles.Add(CreateStyleInclude(source)));
+        }
+    }
+
+    private XElement CreateStyleInclude(string source) =>
+        new(AvaloniaNamespace + "StyleInclude",
+            new XAttribute("Source", source));
+
+    private void SaveStylesToFile(XElement styles)
+    {
+        var settings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = "    ",
+            NewLineChars = Environment.NewLine,
+            NewLineHandling = NewLineHandling.Replace,
+            NewLineOnAttributes = true,
+            OmitXmlDeclaration = true
+        };
+
+        using var writer = XmlWriter.Create(OutputFileName, settings);
+        styles.Save(writer);
+    }
+
+    private class CategoryInfo(string comment)
+    {
+        public string Comment { get; } = comment;
+        public List<string> Sources { get; } = [];
     }
 }
--- src/MuseDashModTools.Tasks/GlobalUsings.cs
+++ src/MuseDashModTools.Tasks/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using System.Text;
+﻿global using System.Xml;
+global using System.Xml.Linq;
 global using JetBrains.Annotations;
 global using Microsoft.Build.Framework;
 global using Task = Microsoft.Build.Utilities.Task;
