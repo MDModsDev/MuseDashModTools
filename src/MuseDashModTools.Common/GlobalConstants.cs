48abf9d491ea8263f9a3f1527f8ff4d5c68ed680
lxymahatma <lxymahatma@gmail.com>
2025-04-25 21:26:44 +0900

    use iconbutton instead of imagebutton

--- src/MuseDashModTools.Common/GlobalConstants.cs
+++ src/MuseDashModTools.Common/GlobalConstants.cs
@@ -6,4 +6,6 @@ public static class GlobalConstants
     public const string MelonLoaderVersion = "0.6.1";
     public const string Cpp2ILVersion = "2022.1.0-pre-release.10";
     public const string UnityDependencyVersion = "2019.4.32";
+    public const string DotNetVersion = "9.0";
+    public const string AvaloniaVersion = "11.2.8";
 }
