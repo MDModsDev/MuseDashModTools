81a148ee589d805e76795e98530aebf233d8b522
lxymahatma <lxymahatma@gmail.com>
2025-06-10 20:25:16 +0900

    remove everything aot incompatible

--- src/MuseDashModTools.Abstractions/Serializations/IJsonSerializationService.cs
+++ src/MuseDashModTools.Abstractions/Serializations/IJsonSerializationService.cs
@@ -1,43 +1,7 @@
-using System.Text.Json;
-
 namespace MuseDashModTools.Abstractions;
 
-public interface IJsonSerializationService : ISerializationService
+public interface IJsonSerializationService
 {
-    #region AOT Compatible
-
     ValueTask<Config?> DeserializeConfigAsync(Stream utf8Json, CancellationToken cancellationToken = default);
     Task SerializeConfigAsync(Stream utf8Json, Config value, CancellationToken cancellationToken = default);
-
-    #endregion AOT Compatible
-
-    #region AOT Incompatible
-
-    T? Deserialize<T>(string json, JsonSerializerOptions? options);
-    T? DeserializeIndented<T>(string json);
-
-    ValueTask<T?> DeserializeAsync<T>(
-        Stream utf8Json,
-        JsonSerializerOptions? options = null,
-        CancellationToken cancellationToken = default);
-
-    ValueTask<T?> DeserializeIndentedAsync<T>(
-        Stream utf8Json,
-        CancellationToken cancellationToken = default);
-
-    string Serialize<T>(T value, JsonSerializerOptions? options);
-    string SerializeIndented<T>(T value);
-
-    Task SerializeAsync<T>(
-        Stream utf8Json,
-        T value,
-        JsonSerializerOptions? options = null,
-        CancellationToken cancellationToken = default);
-
-    Task SerializeIndentedAsync<T>(
-        Stream utf8Json,
-        T value,
-        CancellationToken cancellationToken = default);
-
-    #endregion AOT Incompatible
 }
--- tests/MuseDashModTools.Tests/JsonSerializationServiceTest.cs
+++ tests/MuseDashModTools.Tests/JsonSerializationServiceTest.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace MuseDashModTools.Tests;
 
 [TestSubject(typeof(JsonSerializationService))]
@@ -39,52 +37,4 @@ public sealed class JsonSerializationServiceTest
         IncompatibleMods = [],
         SHA256 = "9fa86686c2a2f256d052e5319b0e7fded0d1ba9a95fd35113d499a28663b40e7"
     };
-
-    [Test]
-    public async Task DeserializeTest()
-    {
-        var result = _jsonSerializationService.Deserialize<Mod>(ModJson);
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task DeserializeIndentedTest()
-    {
-        var result = _jsonSerializationService.DeserializeIndented<Mod>(ModJson);
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task DeserializeAsyncTest()
-    {
-        var memoryStream = new MemoryStream();
-        await memoryStream.WriteAsync(Encoding.UTF8.GetBytes(ModJson).AsMemory());
-        memoryStream.Position = 0;
-        var result = await _jsonSerializationService.DeserializeAsync<Mod>(memoryStream);
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task SerializeTest()
-    {
-        var result = _jsonSerializationService.Serialize(_mod);
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task SerializeAsyncTest()
-    {
-        var memoryStream = new MemoryStream();
-        await _jsonSerializationService.SerializeAsync(memoryStream, _mod);
-        memoryStream.Position = 0;
-        var result = await new StreamReader(memoryStream).ReadToEndAsync();
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task SerializeIndentedTest()
-    {
-        var result = _jsonSerializationService.SerializeIndented(_mod);
-        await Verify(result);
-    }
 }
