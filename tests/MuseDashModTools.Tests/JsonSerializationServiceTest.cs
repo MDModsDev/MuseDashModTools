9d9ee8d94edf7f90371af72754ec0d01d0ce42fa
lxymahatma <lxymahatma@gmail.com>
2025-07-06 02:20:29 +0900

    json serialization test update

--- tests/MuseDashModTools.Tests/JsonSerializationServiceTest.cs
+++ tests/MuseDashModTools.Tests/JsonSerializationServiceTest.cs
@@ -1,40 +1,62 @@
+using System.Text;
+using MuseDashModTools.Models.Enums;
+
 namespace MuseDashModTools.Tests;
 
 [TestSubject(typeof(JsonSerializationService))]
 public sealed class JsonSerializationServiceTest
 {
-    private const string ModJson = """
-                                   {
-                                       "Name": "TestMod",
-                                       "Version": "1.0.0-beta",
-                                       "Author": "test",
-                                       "FileName": "Test Mod.dll",
-                                       "Repository": "Org/Repo",
-                                       "ConfigFile": "Config.cfg",
-                                       "GameVersion": "*",
-                                       "Description": "A Test Mod",
-                                       "ModDependencies": [],
-                                       "LibDependencies": [],
-                                       "IncompatibleMods": [],
-                                       "SHA256": "9fa86686c2a2f256d052e5319b0e7fded0d1ba9a95fd35113d499a28663b40e7"
-                                   }
-                                   """;
+    private const string ConfigJson = """
+                                      {
+                                          "MuseDashFolder": "SteamLibrary\\steamapps\\common\\Muse Dash",
+                                          "CacheFolder": "Cache",
+                                          "LanguageCode": "zh-Hans",
+                                          "Theme": "Dark",
+                                          "ShowConsole": true,
+                                          "AlwaysShowScrollBar": true,
+                                          "DownloadSource": "GitHub",
+                                          "UpdateSource": "GitHubRSS",
+                                          "GitHubToken": null,
+                                          "CustomDownloadSource": null,
+                                          "DownloadPrerelease": false,
+                                          "SkipVersion": null,
+                                          "IgnoreException": false
+                                      }
+                                      """;
 
     private readonly JsonSerializationService _jsonSerializationService = new();
 
-    private readonly Mod _mod = new()
+    [Test]
+    public async Task SerializeConfig_ShouldReturnValidJson()
     {
-        Name = "TestMod",
-        Version = "1.0.0-beta",
-        Author = "test",
-        FileName = "Test Mod.dll",
-        Repository = "Org/Repo",
-        ConfigFile = "Config.cfg",
-        GameVersion = "*",
-        Description = "A Test Mod",
-        ModDependencies = [],
-        LibDependencies = [],
-        IncompatibleMods = [],
-        SHA256 = "9fa86686c2a2f256d052e5319b0e7fded0d1ba9a95fd35113d499a28663b40e7"
-    };
+        var config = new Config
+        {
+            MuseDashFolder = "SteamLibrary\\steamapps\\common\\Muse Dash",
+            CacheFolder = "Cache",
+            LanguageCode = "zh-Hans",
+            Theme = "Dark",
+            ShowConsole = true,
+            AlwaysShowScrollBar = true,
+            DownloadSource = DownloadSource.GitHub,
+            UpdateSource = UpdateSource.GitHubRSS,
+            GitHubToken = null,
+            CustomDownloadSource = null,
+            DownloadPrerelease = false,
+            SkipVersion = null,
+            IgnoreException = false
+        };
+
+        var stream = new MemoryStream();
+        await _jsonSerializationService.SerializeConfigAsync(stream, config);
+        await Verify(stream, "json");
+    }
+
+    [Test]
+    public async Task DeserializeConfig_ShouldReturnValidConfig()
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(ConfigJson));
+        var config = await _jsonSerializationService.DeserializeConfigAsync(stream);
+
+        await Verify(config);
+    }
 }
