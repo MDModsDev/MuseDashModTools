005d6c70082399288e4862c979fb9ad87afbcd14
lxymahatma <lxymahatma@gmail.com>
2025-08-22 23:27:27 +0800

    add SemVersionConverter

---
+++ src/MuseDashModTools.Core/Converters/SemVersionConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MuseDashModTools.Core.Converters;
+
+public sealed class SemVersionConverter : JsonConverter<SemVersion>
+{
+    public override SemVersion? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var versionString = reader.GetString();
+        return versionString is null ? null : SemVersion.Parse(versionString);
+    }
+
+    public override void Write(Utf8JsonWriter writer, SemVersion value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
--- src/MuseDashModTools.Core/JsonContexts/PascalCaseJsonContext.cs
+++ src/MuseDashModTools.Core/JsonContexts/PascalCaseJsonContext.cs
@@ -1,7 +1,8 @@
 ﻿using System.Text.Json.Serialization;
+using MuseDashModTools.Core.Converters;
 
 namespace MuseDashModTools.Core.JsonContexts;
 
-[JsonSourceGenerationOptions(WriteIndented = true, IndentCharacter = ' ', IndentSize = 4)]
+[JsonSourceGenerationOptions(WriteIndented = true, IndentCharacter = ' ', IndentSize = 4, Converters = [typeof(SemVersionConverter)])]
 [JsonSerializable(typeof(Config))]
 internal sealed partial class PascalCaseJsonContext : JsonSerializerContext;
